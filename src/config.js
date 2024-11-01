c6988dbd849de2e40df3328e5ac87c9dee0d551c
yashdani99 <yashdani99@gmail.com>
2024-10-31 20:57:02 -0700

    Update frontend routes

--- src/config.js
+++ src/config.js
@@ -3,74 +3,74 @@ export const BASE_URL = "http://localhost:5001";
 export const API_ENDPOINTS = {
 
     //mission-menu-manager.js
-    createUpdateMissionPlan: "/api/createMissionPlanNew",
+    createUpdateMissionPlan: "/api/v1/droneScheduler/createMissionPlanNew",
     //cesium.js, modifyMission/index.jsx
-    getOneMissionForPlanner: "/api/getonemissionforplanner",
+    getOneMissionForPlanner: "/api/v1/droneScheduler/getonemissionforplanner",
     //LoginPage.js
-    getUserProfile: "/api/getuserProfile",
-    login: "/api/login",
+    getUserProfile: "/api/v1/droneScheduler/getuserProfile",
+    login: "/api/v1/droneScheduler/login",
     //MapBoxDynamic.jsx
-    getLineCoordsForMission: "/api/getlinecoordsformission",
+    getLineCoordsForMission: "/api/v1/droneScheduler/getlinecoordsformission",
     //MissionDetailsCard.jsx
-    getMissionsForUser: "/api/getmissionsforuser",
+    getMissionsForUser: "/api/v1/droneScheduler/getmissionsforuser",
     //addMap/index.jsx, addMap2/index.jsx
-    addMap: "/api/addMap",
+    addMap: "/api/v1/droneScheduler/addMap",
     //createDrone/index.jsx
-    addDrone: "/api/adddrone",
+    addDrone: "/api/v1/droneScheduler/adddrone",
     //createMission/index.jsx, modifyMission/index.jsx
-    getServiceTypesForAllDrones: "/api/getservicetypesforalldrones1",
+    getServiceTypesForAllDrones: "/api/v1/droneScheduler/getservicetypesforalldrones1",
     //createMission2/index.jsx, deleteMapByName/index.jsx
-    getAllMaps: "/api/getAllMaps",
-    createMissionPlan: "/api/createMissionPlan",
+    getAllMaps: "/api/v1/droneScheduler/getAllMaps",
+    createMissionPlan: "/api/v1/droneScheduler/createMissionPlan",
     //createSchedule/index.jsx
-    fetchMissionOptions: "/api/missionOptions",
-    fetchDroneOptions: "/api/droneOptions",
-    addSchedule: "/api/addschedule",
+    fetchMissionOptions: "/api/v1/droneScheduler/missionOptions",
+    fetchDroneOptions: "/api/v1/droneScheduler/droneOptions",
+    addSchedule: "/api/v1/droneScheduler/addschedule",
     //dashboard/index.jsx
-    countDrones: "/api/countdrones",
-    countUsers: "/api/countusers",
-    countMissions: "/api/countmissions",
-    getAllMissionPlans: "/api/getAllMissionPlans",
+    countDrones: "/api/v1/droneScheduler/countdrones",
+    countUsers: "/api/v1/droneScheduler/countusers",
+    countMissions: "/api/v1/droneScheduler/countmissions",
+    getAllMissionPlans: "/api/v1/droneScheduler/getAllMissionPlans",
     //deleteAllMaps/index.jsx
-    deleteAllMaps: "/api/deleteAllMaps",
+    deleteAllMaps: "/api/v1/droneScheduler/deleteAllMaps",
     //deleteAllMissions/index.jsx
-    deleteAllMissionPlans: "/api/deleteAllMissionPlans",
+    deleteAllMissionPlans: "/api/v1/droneScheduler/deleteAllMissionPlans",
     //deleteMapByName/index.jsx
-    deleteMapByName: "/api/deleteMapByName",
+    deleteMapByName: "/api/v1/droneScheduler/deleteMapByName",
     //deleteMissionById/index.jsx
-    deleteMissionPlanById: "/api/deleteMissionPlanById",
+    deleteMissionPlanById: "/api/v1/droneScheduler/deleteMissionPlanById",
     //EditDrone/index.jsx
-    getDrones: "/api/drones",
+    getDrones: "/api/v1/droneScheduler/drones",
     //EditSchedule/index.jsx
-    getSchedules: "/api/schedules",
+    getSchedules: "/api/v1/droneScheduler/schedules",
     //missionDashboard/index.jsx
-    getAllMissionsForGivenUser: "/api/getallmissionsforgivenuser",
+    getAllMissionsForGivenUser: "/api/v1/droneScheduler/getallmissionsforgivenuser",
     //trackingConfiguration/index.jsx
-    configureTracking: "/api/configuretracking",
-    getConfiguredTracking: "/api/getconfiguredtracking",
+    configureTracking: "/api/v1/droneScheduler/configuretracking",
+    getConfiguredTracking: "/api/v1/droneScheduler/getconfiguredtracking",
     //trackingDashboard/index.jsx
-    getAllDronesPerUserForMap: "/api/getalldronesperuserformap",
-    getRecentNotifications : "/api/getrecentnotifications",
+    getAllDronesPerUserForMap: "/api/v1/droneScheduler/getalldronesperuserformap",
+    getRecentNotifications : "/api/v1/droneScheduler/getrecentnotifications",
     //trackingDrone/index.jsx
-    getOneDrone: "/api/getonedrone1",
-    getOneMission: "/api/getonemission1",
-    getNotificationsForMission: "/api/getnotificationsformission",
+    getOneDrone: "/api/v1/droneScheduler/getonedrone1",
+    getOneMission: "/api/v1/droneScheduler/getonemission1",
+    getNotificationsForMission: "/api/v1/droneScheduler/getnotificationsformission",
     //VideoDashboard.js, VideoDashboard2.js
-    videoList: "/api/videoList",
-    getVideos: "/api/videos",
+    videoList: "/api/v1/droneScheduler/videoList",
+    getVideos: "/api/v1/droneScheduler/videos",
     //VideoUpload.js
-    videoUpload: "/api/upload",
+    videoUpload: "/api/v1/droneScheduler/upload",
     //viewDrone/index.jsx
-    getAllDrones: "/api/getalldrones1",
+    getAllDrones: "/api/v1/droneScheduler/getalldrones1",
     //viewDrone2/index.jsx
-    viewDrone : "/api/viewdrone",
+    viewDrone : "/api/v1/droneScheduler/viewdrone",
     //viewMissions/index.jsx
-    deleteMissionForUser: "/api/deletemissionforuser",
-    deleteConfigurationForMission: "/api/deleteconfigurationformission",
-    deleteNotificationsForMission: "/api/deletenotificationsformission",
+    deleteMissionForUser: "/api/v1/droneScheduler/deletemissionforuser",
+    deleteConfigurationForMission: "/api/v1/droneScheduler/deleteconfigurationformission",
+    deleteNotificationsForMission: "/api/v1/droneScheduler/deletenotificationsformission",
     //viewSchedules/index.jsx
-    viewSchedules: "/api/viewschedule",
+    viewSchedules: "/api/v1/droneScheduler/viewschedule",
     //RegisterPage.js
-    signUp: "/api/signup",
+    signUp: "/api/v1/droneScheduler/signup",
   // Add more API endpoints here as needed
 };
